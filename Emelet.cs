f371abfd6a2f7694a7923d138d7ad142eeb15509
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-01-31 11:22:15 +0100

    Add project files.

---
+++ Emelet.cs
@@ -0,0 +1,27 @@
+﻿namespace CA241031
+{
+    internal class Emelet
+    {
+        public string Nev { get; set; }
+        public Dictionary<int, int> Szektorok  { get; set; }
+
+        public override string ToString()
+        {
+            string str = $"{Nev,-10} | ";
+            foreach (var kvp in Szektorok) str += $"{kvp.Value,3} | ";
+            return str[..^3];
+        }
+            
+
+        public Emelet(string sor)
+        {
+            var v = sor.Split("; ");
+            Nev = v[0];
+            Szektorok = new();
+            for (int i = 1; i < v.Length; i++)
+            {
+                Szektorok.Add(i, int.Parse(v[i]));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using CA241031;
+using System.Text;
+
+List<Emelet> emeletek = new();
+using StreamReader sr = new(@"..\..\..\src\parkolohaz.txt", Encoding.UTF8);
+while (!sr.EndOfStream) emeletek.Add(new(sr.ReadLine()));
+
+Console.WriteLine(" #no | szint neve | sz1 | sz2 | sz3 | sz4 | sz5 | sz6");
+Console.WriteLine("-----------------------------------------------------");
+for (int i = 0; i < emeletek.Count; i++)
+    Console.WriteLine($" {i+1,2}. | {emeletek[i]}");
+Console.Write("\n");
+
+var f08 = emeletek.MinBy(e => e.Szektorok.Values.Sum()).Nev;
+Console.WriteLine($"8.: Legkevesebb parkoló autó: {f08}");
+
+
+
+
